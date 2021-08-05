7ee9f1223cf4622135a93171e68f40f09aa4b432
Tyler Smart <smart@savorymanagement.com>
2021-08-04 19:36:34 -0600

    v1.0.1 - Add variables to production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,51 @@
 export const environment = {
-  production: true
+  production: true,
+  vehicles: [
+    {
+      vin: '3FMCR9D9XMRB06425',
+      order: 'L08P',
+    },
+    {
+      vin: '3FMCR9D91MRB06426',
+      order: 'L09P',
+    },
+    {
+      vin: '3FMCR9D92MRB06421',
+      order: 'L04P',
+    },
+    {
+      vin: '3FMCR9D93MRB06427',
+      order: 'L10P',
+    },
+    {
+      vin: '3FMCR9D90MRB06420',
+      order: 'L03P',
+    },
+    {
+      vin: '3FMCR9D93MRA88611',
+      order: 'C001',
+    },
+    {
+      vin: '3FMCR9D94MRA88858',
+      order: 'T005',
+    },
+    {
+      vin: '3FMCR9D94MRB06419',
+      order: 'L02P',
+    },
+    {
+      vin: '3FMCR9D96MRB06423',
+      order: 'L06P',
+    },
+    {
+      vin: '3FMCR9D98MRB06424',
+      order: 'L07P',
+    },
+    {
+      vin: '3FMCR9D94MRB06422',
+      order: 'L05P',
+    },
+  ],
+  etisToken: '4a8ab0635a98a26bda3e975585a0776d',
+  api: `http://${window.location.hostname}:4000`,
 };
